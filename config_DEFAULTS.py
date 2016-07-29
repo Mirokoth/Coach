bc46bd03b090b0292ddf7949311f091b530ec8b5
Mirokoth <mail@rhy.se>
2016-07-30 09:57:19 +1000

    Add files via upload

--- config_DEFAULTS.py
+++ config_DEFAULTS.py
@@ -1,10 +1,9 @@
 # config-defaults
 # note: rename this file to 'config.py' to override
 
-BOT_TOKEN = "MjA4NTc1NTg0Njk0MDQyNjI1.Cnzqpg.B9_cAwkgnDs3ZATJhTId2jDeK0k" # Discord API token
-OWM_TOKEN = "c70f0e635539159ab87dc1b61f812d5d" # Pyowm API token
-BOT_CMD_SYMBOL = "!" # Symbol prefix required for bot commands
-PYTHON_CMD = "python3" # Python command depending on environment (python/python2/python3)
-COMMAND_LOG = "command_log.txt" # Log file
-CHAL_USER = "Mirokoth" # Challonge username
-CHAL_API = "PYMUs8sFT7NKoAlnloBxK4ZuAmPtgAPttDtQJU5a" # Challonge API
+BOT_TOKEN = "ENTER DISCORD BOT API" # Discord API token
+BOT_CMD_SYMBOL = "SELECT SYMBOL BOT USES FOR COMMANDS" # Symbol prefix required for bot commands
+PYTHON_CMD = "DEFAULT PYTHON VERIABLE" # Python command depending on environment (python/python2/python3)
+COMMAND_LOG = "OUTPUT FOR LOGS" # Log file
+CHAL_USER = "CHALLONGE USERNAME" # Challonge username
+CHAL_API = "CHALLONG API KEY" # Challonge API
