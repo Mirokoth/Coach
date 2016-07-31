b9e1a43250f79f2ab6b0ff9ccb797b126f72283e
Rhyse Allan <mail@rhy.se>
2016-07-31 21:13:08 +1000

    MATCHES cleanup
    Cleaned up argument checking for matches
    Removed Variable referencing removed module

--- Coach_Lag.py
+++ Coach_Lag.py
@@ -19,7 +19,6 @@ BOT_CMD_SYMBOL = config.BOT_CMD_SYMBOL
 client = discord.Client()
 roles = []
 server_lst = []
-sched = AsyncIOScheduler()
 
 # Challonge Credentials
 challonge.set_credentials(config.CHAL_USER, config.CHAL_API)
@@ -134,7 +133,10 @@ async def on_message(message):
 
 
     if command == "MATCHES": # Gets match details for provided Tournament
-        try:
+
+        if arguments == False:
+            await client.send_message(message.channel, "No Tournament details or command switches provided")
+        elif len(arguments) > 1:
             if arguments[1].upper() == "WINNER": # If second argument given is winner provide only match win details
                 argument_count = 0 # Count how many matches have been won so far
                 for match in challonge.matches.index(arguments[0]):
@@ -146,7 +148,7 @@ async def on_message(message):
                 # If no matches won yet, return information
                 if argument_count <= 0:
                     await client.send_message(server_lst[0], 'No matches have been won yet.')
-        except IndexError:
+        elif len(arguments) == 1:
             for match in challonge.matches.index(arguments[0]):
                 output = ""  # String to send
                 for details in match:
