92afdae67e1bb3fff69f5b49b76740bec05a2ba1
Kosta Kazias <kostakazias@gmail.com>
2016-07-31 21:21:24 +1000

    Move config module into /config

---
+++ config/config_DEFAULTS.py
@@ -0,0 +1,11 @@
+# config-defaults
+# note: rename this file to 'config.py' to override
+
+BOT_TOKEN = "ENTER DISCORD BOT API" # Discord API token
+BOT_CMD_SYMBOL = "SELECT SYMBOL BOT USES FOR COMMANDS" # Symbol prefix required for bot commands
+PYTHON_CMD = "DEFAULT PYTHON VERIABLE" # Python command depending on environment (python/python2/python3)
+COMMAND_LOG = "OUTPUT FOR LOGS" # Log file
+CHAL_USER = "CHALLONGE USERNAME" # Challonge username
+CHAL_API = "CHALLONG API KEY" # Challonge API
+G_API = 'API FILE NAME'# API .JSON file for Google
+GSHEET_URL = "URL" # URL for the Google Docs spreedsheet
