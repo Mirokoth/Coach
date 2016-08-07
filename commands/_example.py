b139856af8bce75f59ad0f472c6bc4bb882f7ff3
Kosta Kazias <kostakazias@gmail.com>
2016-08-07 13:29:19 +1000

    Update example module

---
+++ commands/_example.py
@@ -0,0 +1,11 @@
+import asyncio
+
+class Example():
+    def __init__(self, message_handler, coach):
+        self.message_handler = message_handler
+        self.coach = coach
+
+    # Command received
+    async def on_message(self, message, command, arguments):
+        res = 'This is a help file?\nCommand was {} and arguments {}'.format(command, arguments);
+        return await self.coach.forward_message(message.channel, res)
